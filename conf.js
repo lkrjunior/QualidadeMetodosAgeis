2e0a6ce39efdb2d32414cd0dce7d678c84a95cdd
LUCIANO JUNIOR KRUSE ROCHA <ALU201830279@uniritter.local>
2019-03-29 21:00:04 -0300

    Ajustando travis

--- conf.js
+++ conf.js
@@ -2,9 +2,10 @@ exports.config = {
 	framework: 'jasmine', 
 	seleniumAddress: 'http://localhost:4444/wd/hub/',
 	specs: ['spec.js'],
-capabilities: {
-    browserName: 'chrome',
-    chromeOptions: {
-     args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
-   }
+    capabilities: {
+		browserName: 'chrome',
+		chromeOptions: {
+			args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+		}
+	}
 }
